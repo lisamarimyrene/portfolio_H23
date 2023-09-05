4c879326ef925fad2a6402e9612c427ed62e3f17
lisamarimyrene <lisamy@stud.ntnu.no>
2023-09-05 23:33:10 +0200

    router dom

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+
+export default function Homepage() {
+    return (
+        <div className="min-h-screen bg-pink">
+            <section>
+                <div></div>
+                <div></div>
+                <h1 className="font-anton text-5xl">It's Lisa</h1>
+            </section>
+            <section>
+
+            </section>
+        </div>
+    )
+}
---
+++ src/pages/Skillspage.jsx
@@ -0,0 +1,15 @@
+
+export default function Skillspage() {
+    return (
+        <div className="min-h-screen bg-pink">
+            <section>
+                <div></div>
+                <div></div>
+                <h1 className="font-anton text-5xl">It's Lisa</h1>
+            </section>
+            <section>
+
+            </section>
+        </div>
+    )
+}
