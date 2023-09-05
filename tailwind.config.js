64f7c864182bdc8b240de1eda7a2fa1842157e89
lisamarimyrene <lisamy@stud.ntnu.no>
2023-09-05 22:53:41 +0200

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    colors: {
+      gray: {
+        dark: '#4F4F4F',
+        medium: '#ABABAB',
+        light: '#E5E5E6',
+      },
+      pink: '#FFB1CB',
+      purple: '#453D55'
+    },
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'anton': ['Anton', 'sans-serif']
+    },
   },
   plugins: [],
 }
