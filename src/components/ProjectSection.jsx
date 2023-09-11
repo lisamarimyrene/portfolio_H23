223bb208b062b7b3336c77fdb08851070d812c48
lisamarimyrene <lisamy@stud.ntnu.no>
2023-09-11 19:43:58 +0200

    fixed margin

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -30,7 +30,7 @@ export const ProjectSection = () => {
             {projects && projects.map((project, i) => i < 3 && <Project side={i % 2 === 0 ? 'left' : 'right'} id={'0' + (i + 1)} projectData={project} />)}
 
             <div className="flex flex-col md:flex-row md:justify-evenly md:items-center md:my-32">
-                <img className="hover:animate-pulse rotate-[-60deg] md:rotate-[-90deg] -my-10 md:h-[20rem] md:order-last md:my-0" src={glass_spiral} alt="Glass spiral." />
+                <img className="hover:animate-pulse rotate-[-60deg] md:rotate-[-90deg] -my-5 md:h-[20rem] md:order-last md:my-0" src={glass_spiral} alt="Glass spiral." />
                 <div className="flex flex-col items-center pb-6 md:pb-0 md:w-[40%] md:-ml-[8rem] md:text-lg md:leading-8">
                     <p className="z-10 w-3/5 text-justify">My name is Lisa Mari Myrene, and I'm in my third year studying Web Development at NTNU Gjøvik. I have always had an interest in Web Development and Design, and are therefore currently learning a larger aspect of the profession, both Front-End and Back-End oriented. I am currently extending my horizon by taking an exchange semester at CODE University in Berlin. Now I'm looking for a workplace where I can share my skills in the future.</p>
                     <p className="z-10 text-5xl md:text-6xl py-8 md:py-10 hover:animate-spin">🪩</p>
