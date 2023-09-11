4e99b76ad9e8a3ffdcbac1ae658415f278862c40
lisamarimyrene <lisamy@stud.ntnu.no>
2023-09-11 19:24:17 +0200

    created footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,13 @@
 import { ProjectSection } from "../components/ProjectSection"
 import { HeroSection } from "../components/HeroSection"
+import { Footer } from "../components/Footer"
 
 export default function Homepage() {
     return (
         <div className="min-h-screen">
             <HeroSection />
             <ProjectSection />
+            <Footer />
         </div>
     )
 }
